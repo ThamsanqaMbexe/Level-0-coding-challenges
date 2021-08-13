49f6b53d1ab6b415f8ec260ff6a5a933cac3db41
Thamsanqa Mbexe <thamsanqambexe@gmail.com>
2021-08-13 21:01:20 +0200

    level 0 coding challenges

---
+++ level-06.js
@@ -0,0 +1,11 @@
+function maxNum(a, b, c) {
+    if( a > (b && c)) {
+        return a;
+    } else if (b > (a && c)) {
+        return b;
+    } else if (c > (a && b)) {
+        return c;
+    } else {
+        return "all even";
+    }
+}
