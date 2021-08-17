f82c5da9706b458c75768f7100c807f99e1c9cde
ThamsanqaMbexe <87939065+ThamsanqaMbexe@users.noreply.github.com>
2021-08-17 04:14:10 +0200

    Add files via upload
    completed task.

---
+++ level-09.js
@@ -0,0 +1,13 @@
+function vowelOutput(str) {
+    var vowelArray = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    var vowels = "";
+
+    for(i = 0; i < str.length; i++) {
+        if(vowelArray.includes(str[i])) {
+            vowels += str[i].toLowerCase();            
+        }
+
+    }
+    var oneLetter = [...new Set(vowels)];
+    console.log("Vowels: " + oneLetter.join(", "));
+}
