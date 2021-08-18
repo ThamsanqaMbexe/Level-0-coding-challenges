68704d3d491590e33e903409aeab189924eb4c55
ThamsanqaMbexe <87939065+ThamsanqaMbexe@users.noreply.github.com>
2021-08-18 03:10:36 +0200

    Update level-10.js
    Fixed the bug to output all common char.

--- level-10.js
+++ level-10.js
@@ -2,8 +2,10 @@ function commonChar(str1, str2) {
     var common = "";
 
     for(i = 0; i < str1.length; i++) {
-        if(str1.includes(str2[i])) {
-            common += str2[i].toLowerCase();            
+        for(j = 0; j < str2.length; j++) {
+            if(str1.includes(str2[j])) {
+                common += str2[j].toLowerCase();            
+            }
         }
     }
     var oneLetter = [...new Set(common)];
