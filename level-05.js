380a81e6e2d39b6fae2de0effe7bd144c9ff2437
ThamsanqaMbexe <87939065+ThamsanqaMbexe@users.noreply.github.com>
2021-08-18 11:00:35 +0200

    Update level-05.js
    Logical error fixed.

--- level-05.js
+++ level-05.js
@@ -1,5 +1,5 @@
 function triangleArea(a, b, c) {
     var semiPeram = (a + b + c) / 2;
-    var area = Math.sqrt(semiPeram * (semiPeram - a) * (semiPeram - b) * (semiPeram - b));
+    var area = Math.sqrt(semiPeram * ((semiPeram - a) * (semiPeram - b) * (semiPeram - b)));
     return area;
 }
