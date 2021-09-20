13ee7c5c1c3e8792fde327adb44d687052d3df5e
Thamsanqa Mbexe <thamsanqambexe@gmail.com>
2021-09-20 22:07:31 +0200

    used better naming convention for paremeters

--- level-05.js
+++ level-05.js
@@ -1,5 +1,5 @@
-function triangleArea(a, b, c) {
-    let semiPeram = (a + b + c) / 2;
-    let area = Math.sqrt(semiPeram * ((semiPeram - a) * (semiPeram - b) * (semiPeram - b)));
+function triangleArea(s1, s2, s3) {
+    let semiPeram = (s1 + s2 + s3) / 2;
+    let area = Math.sqrt(semiPeram * ((semiPeram - s1) * (semiPeram - s2) * (semiPeram - s3)));
     return area;
 }
