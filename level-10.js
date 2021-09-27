116e931b649282d7d579023987fc6736e43cefa7
ThamsanqaMbexe <87939065+ThamsanqaMbexe@users.noreply.github.com>
2021-09-27 13:40:57 +0200

    Update level-10.js
    declared i and j.

--- level-10.js
+++ level-10.js
@@ -1,8 +1,8 @@
 function commonLetters(str1, str2) {
     let common = "";
 
-    for(i = 0; i < str1.length; i++) {
-        for(j = 0; j < str2.length; j++) {
+    for(let i = 0; i < str1.length; i++) {
+        for(let j = 0; j < str2.length; j++) {
             if(str1.includes(str2[j])) {
                 common += str2[j].toLowerCase();            
             }
